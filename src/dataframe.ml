bd97dfaf2559ab498da76a59cf39d39ff3f236d4
Vincent Bernardoff <vb@luminar.eu.org>
2019-10-17 20:06:17 +0800

    add list module

--- src/dataframe.ml
+++ src/dataframe.ml
@@ -3,57 +3,8 @@ type _ t =
   | Cols : 'a t * 'b t -> ('a * 'b) t
   | Conv : ('a -> 'b) * ('b -> 'a) * 'b t -> 'a t
 
+let merge_tups a b = Cols (a, b)
 let conv ffrom fto v = Conv (ffrom, fto, v)
-
-let t1 a = Col a
-let t2 a b = Cols (Col a, Col b)
-let t3 a b c =
-  conv
-    (fun (a, b, c) -> a, (b, c))
-    (fun (a, (b, c)) -> a, b, c)
-    (Cols (Col a, t2 b c))
-
-let t4 a b c d =
-  conv
-    (fun (a, b, c, d) -> a, (b, c, d))
-    (fun (a, (b, c, d)) -> a, b, c, d)
-    (Cols (Col a, t3 b c d))
-
-let t5 a b c d e =
-  conv
-    (fun (a, b, c, d, e) -> a, (b, c, d, e))
-    (fun (a, (b, c, d, e)) -> a, b, c, d, e)
-    (Cols (Col a, t4 b c d e))
-
-let t6 a b c d e f =
-  conv
-    (fun (a, b, c, d, e, f) -> a, (b, c, d, e, f))
-    (fun (a, (b, c, d, e, f)) -> a, b, c, d, e, f)
-    (Cols (Col a, t5 b c d e f))
-
-let t7 a b c d e f g =
-  conv
-    (fun (a, b, c, d, e, f, g) -> a, (b, c, d, e, f, g))
-    (fun (a, (b, c, d, e, f, g)) -> a, b, c, d, e, f, g)
-    (Cols (Col a, t6 b c d e f g))
-
-let t8 a b c d e f g h =
-  conv
-    (fun (a, b, c, d, e, f, g, h) -> a, (b, c, d, e, f, g, h))
-    (fun (a, (b, c, d, e, f, g, h)) -> a, b, c, d, e, f, g, h)
-    (Cols (Col a, t7 b c d e f g h))
-
-let t9 a b c d e f g h i =
-  conv
-    (fun (a, b, c, d, e, f, g, h, i) -> a, (b, c, d, e, f, g, h, i))
-    (fun (a, (b, c, d, e, f, g, h, i)) -> a, b, c, d, e, f, g, h, i)
-    (Cols (Col a, t8 b c d e f g h i))
-
-let t10 a b c d e f g h i j =
-  conv
-    (fun (a, b, c, d, e, f, g, h, i, j) -> a, (b, c, d, e, f, g, h, i, j))
-    (fun (a, (b, c, d, e, f, g, h, i, j)) -> a, b, c, d, e, f, g, h, i, j)
-    (Cols (Col a, t9 b c d e f g h i j))
 
 let rec length : type a. a t -> int = function
   | Col a -> Array.length a
@@ -85,3 +36,45 @@ let fold : type a. ('b -> a -> 'b) -> 'b -> a t -> 'b = fun f a t ->
     acc := f !acc (row t i)
   done ;
   !acc
+
+module type C = sig
+  type 'a c
+  val t1 : 'a c -> 'a t
+  val t2 : 'a c -> 'b c -> ('a * 'b) t
+  val t3 : 'a c -> 'b c -> 'c c -> ('a * 'b * 'c) t
+  val t4 : 'a c -> 'b c -> 'c c -> 'd c -> ('a * 'b * 'c * 'd) t
+  val t5 : 'a c -> 'b c -> 'c c -> 'd c -> 'e c -> ('a * 'b * 'c * 'd * 'e) t
+  val t6 : 'a c -> 'b c -> 'c c -> 'd c -> 'e c -> 'f c -> ('a * 'b * 'c * 'd * 'e * 'f) t
+  val t7 : 'a c -> 'b c -> 'c c -> 'd c -> 'e c -> 'f c -> 'g c -> ('a * 'b * 'c * 'd * 'e * 'f * 'g) t
+  val t8 : 'a c -> 'b c -> 'c c -> 'd c -> 'e c -> 'f c -> 'g c -> 'h c -> ('a * 'b * 'c * 'd * 'e * 'f * 'g * 'h) t
+  val t9 : 'a c -> 'b c -> 'c c -> 'd c -> 'e c -> 'f c -> 'g c -> 'h c -> 'i c -> ('a * 'b * 'c * 'd * 'e * 'f * 'g * 'h * 'i) t
+  val t10 :'a c -> 'b c -> 'c c -> 'd c -> 'e c -> 'f c -> 'g c -> 'h c -> 'i c -> 'j c -> ('a * 'b * 'c * 'd * 'e * 'f * 'g * 'h * 'i * 'j) t
+end
+
+module A = struct
+  let t1 a = Col a
+  let t2 a b = Cols (Col a, Col b)
+  let t3 a b c = conv (fun (a, b, c) -> a, (b, c)) (fun (a, (b, c)) -> a, b, c) (Cols (Col a, t2 b c))
+  let t4 a b c d = conv (fun (a, b, c, d) -> a, (b, c, d)) (fun (a, (b, c, d)) -> a, b, c, d) (Cols (Col a, t3 b c d))
+  let t5 a b c d e = conv (fun (a, b, c, d, e) -> a, (b, c, d, e)) (fun (a, (b, c, d, e)) -> a, b, c, d, e) (Cols (Col a, t4 b c d e))
+  let t6 a b c d e f = conv  (fun (a, b, c, d, e, f) -> a, (b, c, d, e, f)) (fun (a, (b, c, d, e, f)) -> a, b, c, d, e, f) (Cols (Col a, t5 b c d e f))
+  let t7 a b c d e f g = conv (fun (a, b, c, d, e, f, g) -> a, (b, c, d, e, f, g)) (fun (a, (b, c, d, e, f, g)) -> a, b, c, d, e, f, g) (Cols (Col a, t6 b c d e f g))
+  let t8 a b c d e f g h = conv (fun (a, b, c, d, e, f, g, h) -> a, (b, c, d, e, f, g, h)) (fun (a, (b, c, d, e, f, g, h)) -> a, b, c, d, e, f, g, h) (Cols (Col a, t7 b c d e f g h))
+  let t9 a b c d e f g h i = conv (fun (a, b, c, d, e, f, g, h, i) -> a, (b, c, d, e, f, g, h, i)) (fun (a, (b, c, d, e, f, g, h, i)) -> a, b, c, d, e, f, g, h, i) (Cols (Col a, t8 b c d e f g h i))
+  let t10 a b c d e f g h i j = conv (fun (a, b, c, d, e, f, g, h, i, j) -> a, (b, c, d, e, f, g, h, i, j)) (fun (a, (b, c, d, e, f, g, h, i, j)) -> a, b, c, d, e, f, g, h, i, j) (Cols (Col a, t9 b c d e f g h i j))
+end
+
+module L = struct
+  open Array
+  open A
+  let t1 a = t1 (of_list a)
+  let t2 a b = t2 (of_list a) (of_list b)
+  let t3 a b c = t3 (of_list a) (of_list b) (of_list c)
+  let t4 a b c d = t4 (of_list a) (of_list b) (of_list c) (of_list d)
+  let t5 a b c d e = t5 (of_list a) (of_list b) (of_list c) (of_list d) (of_list e)
+  let t6 a b c d e f = t6 (of_list a) (of_list b) (of_list c) (of_list d) (of_list e) (of_list f)
+  let t7 a b c d e f g = t7 (of_list a) (of_list b) (of_list c) (of_list d) (of_list e) (of_list f) (of_list g)
+  let t8 a b c d e f g h = t8 (of_list a) (of_list b) (of_list c) (of_list d) (of_list e) (of_list f) (of_list g) (of_list h)
+  let t9 a b c d e f g h i = t9 (of_list a) (of_list b) (of_list c) (of_list d) (of_list e) (of_list f) (of_list g) (of_list h) (of_list i)
+  let t10 a b c d e f g h i j = t10 (of_list a) (of_list b) (of_list c) (of_list d) (of_list e) (of_list f) (of_list g) (of_list h) (of_list i) (of_list j)
+end
--- src/dataframe.mli
+++ src/dataframe.mli
@@ -1,55 +1,27 @@
 type _ t
+
+val merge_tups : 'a t -> 'b t -> ('a * 'b) t
 val conv : ('a -> 'b) -> ('b -> 'a) -> 'b t -> 'a t
-val t1 : 'a array -> 'a t
-val t2 : 'a array -> 'b array -> ('a * 'b) t
-val t3 : 'a array -> 'b array -> 'c array -> ('a * 'b * 'c) t
-val t4 :
-  'a array -> 'b array -> 'c array -> 'd array -> ('a * 'b * 'c * 'd) t
-val t5 :
-  'a array ->
-  'b array ->
-  'c array -> 'd array -> 'e array -> ('a * 'b * 'c * 'd * 'e) t
-val t6 :
-  'a array ->
-  'b array ->
-  'c array ->
-  'd array -> 'e array -> 'f array -> ('a * 'b * 'c * 'd * 'e * 'f) t
-val t7 :
-  'a array ->
-  'b array ->
-  'c array ->
-  'd array ->
-  'e array -> 'f array -> 'g array -> ('a * 'b * 'c * 'd * 'e * 'f * 'g) t
-val t8 :
-  'a array ->
-  'b array ->
-  'c array ->
-  'd array ->
-  'e array ->
-  'f array ->
-  'g array -> 'h array -> ('a * 'b * 'c * 'd * 'e * 'f * 'g * 'h) t
-val t9 :
-  'a array ->
-  'b array ->
-  'c array ->
-  'd array ->
-  'e array ->
-  'f array ->
-  'g array ->
-  'h array -> 'i array -> ('a * 'b * 'c * 'd * 'e * 'f * 'g * 'h * 'i) t
-val t10 :
-  'a array ->
-  'b array ->
-  'c array ->
-  'd array ->
-  'e array ->
-  'f array ->
-  'g array ->
-  'h array ->
-  'i array ->
-  'j array -> ('a * 'b * 'c * 'd * 'e * 'f * 'g * 'h * 'i * 'j) t
 val length : _ t -> int
 val row : 'a t -> int -> 'a
 val iter : ('a -> unit) -> 'a t -> unit
 val iteri : (int -> 'a -> unit) -> 'a t -> unit
 val fold : ('b -> 'a -> 'b) -> 'b -> 'a t -> 'b
+
+module type C = sig
+  type 'a c
+  val t1 : 'a c -> 'a t
+  val t2 : 'a c -> 'b c -> ('a * 'b) t
+  val t3 : 'a c -> 'b c -> 'c c -> ('a * 'b * 'c) t
+  val t4 : 'a c -> 'b c -> 'c c -> 'd c -> ('a * 'b * 'c * 'd) t
+  val t5 : 'a c -> 'b c -> 'c c -> 'd c -> 'e c -> ('a * 'b * 'c * 'd * 'e) t
+  val t6 : 'a c -> 'b c -> 'c c -> 'd c -> 'e c -> 'f c -> ('a * 'b * 'c * 'd * 'e * 'f) t
+  val t7 : 'a c -> 'b c -> 'c c -> 'd c -> 'e c -> 'f c -> 'g c -> ('a * 'b * 'c * 'd * 'e * 'f * 'g) t
+  val t8 : 'a c -> 'b c -> 'c c -> 'd c -> 'e c -> 'f c -> 'g c -> 'h c -> ('a * 'b * 'c * 'd * 'e * 'f * 'g * 'h) t
+  val t9 : 'a c -> 'b c -> 'c c -> 'd c -> 'e c -> 'f c -> 'g c -> 'h c -> 'i c -> ('a * 'b * 'c * 'd * 'e * 'f * 'g * 'h * 'i) t
+  val t10 :'a c -> 'b c -> 'c c -> 'd c -> 'e c -> 'f c -> 'g c -> 'h c -> 'i c -> 'j c -> ('a * 'b * 'c * 'd * 'e * 'f * 'g * 'h * 'i * 'j) t
+end
+
+module A : C with type 'a c := 'a Array.t
+module L : C with type 'a c := 'a List.t
+
