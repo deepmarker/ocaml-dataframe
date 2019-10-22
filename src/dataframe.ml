53947b16086edf4cb11375fd1dfbb04fe4ba6706
Vincent Bernardoff <vb@luminar.eu.org>
2019-10-22 16:35:40 +0800

    add more functions

--- src/dataframe.ml
+++ src/dataframe.ml
@@ -19,23 +19,45 @@ let rec row : type a. a t -> int -> a = fun t n ->
 
 let iter : type a. (a -> 'b) -> a t -> unit = fun f t ->
   let n = length t in
-  for i = 0 to n - 1 do
-    f (row t i)
-  done
+  for i = 0 to n - 1 do f (row t i) done
 
 let iteri : type a. (int -> a -> 'b) -> a t -> unit = fun f t ->
   let n = length t in
-  for i = 0 to n - 1 do
-    f i (row t i)
-  done
+  for i = 0 to n - 1 do f i (row t i) done
 
-let fold : type a. ('b -> a -> 'b) -> 'b -> a t -> 'b = fun f a t ->
+let rev_iter : type a. (a -> 'b) -> a t -> unit = fun f t ->
+  let n = length t in
+  for i = n - 1 downto 0 do f (row t i) done
+
+let rev_iteri : type a. (int -> a -> 'b) -> a t -> unit = fun f t ->
+  let n = length t in
+  for i = n - 1 downto 0 do f i (row t i) done
+
+let fold_left : type a. ('b -> a -> 'b) -> 'b -> a t -> 'b = fun f a t ->
   let n = length t in
   let acc = ref a in
-  for i = 0 to n - 1 do
-    acc := f !acc (row t i)
-  done ;
+  for i = 0 to n - 1 do acc := f !acc (row t i) done ;
   !acc
+
+let fold_right : type a. (a -> 'b -> 'b) -> a t -> 'b -> 'b = fun f t a ->
+  let n = length t in
+  let acc = ref a in
+  for i = n - 1 downto 0 do acc := f (row t i) !acc done ;
+  !acc
+
+let rev_map : type a. (a -> 'b) -> a t -> 'b list = fun f t ->
+  fold_left (fun a v -> f v :: a) [] t
+
+let map : type a. (a -> 'b) -> a t -> 'b list = fun f t ->
+  fold_right (fun v a -> f v :: a) t []
+
+let rev_mapi : type a. (int -> a -> 'b) -> a t -> 'b list = fun f t ->
+  let _, r = fold_left (fun (i, a) v -> succ i, f i v :: a) (0, []) t in
+  r
+
+let mapi : type a. (int -> a -> 'b) -> a t -> 'b list = fun f t ->
+  let _, r = fold_right (fun v (i, a) -> pred i, f i v :: a) t (pred (length t), []) in
+  r
 
 module type C = sig
   type 'a c
@@ -78,3 +100,50 @@ module L = struct
   let t9 a b c d e f g h i = t9 (of_list a) (of_list b) (of_list c) (of_list d) (of_list e) (of_list f) (of_list g) (of_list h) (of_list i)
   let t10 a b c d e f g h i j = t10 (of_list a) (of_list b) (of_list c) (of_list d) (of_list e) (of_list f) (of_list g) (of_list h) (of_list i) (of_list j)
 end
+
+let c1 ffrom fto a = conv ffrom fto (L.t1 (List.map ffrom a))
+let c2 ffrom fto a =
+  let a, b = List.fold_right (fun elt (a, b) ->
+      let v1, v2 = ffrom elt in
+      v1 :: a, v2 :: b) a ([], []) in
+  conv ffrom fto (L.t2 a b)
+let c3 ffrom fto a =
+  let a, b, c = List.fold_right (fun elt (a, b, c) ->
+      let v1, v2, v3 = ffrom elt in
+      v1 :: a, v2 :: b, v3 :: c) a ([], [], []) in
+  conv ffrom fto (L.t3 a b c)
+let c4 ffrom fto a =
+  let a, b, c, d = List.fold_right (fun elt (a, b, c, d) ->
+      let v1, v2, v3, v4 = ffrom elt in
+      v1 :: a, v2 :: b, v3 :: c, v4 :: d) a ([], [], [], []) in
+  conv ffrom fto (L.t4 a b c d)
+let c5 ffrom fto a =
+  let a, b, c, d, e = List.fold_right (fun elt (a, b, c, d, e) ->
+      let v1, v2, v3, v4, v5 = ffrom elt in
+      v1 :: a, v2 :: b, v3 :: c, v4 :: d, v5 :: e) a ([], [], [], [], []) in
+  conv ffrom fto (L.t5 a b c d e)
+let c6 ffrom fto a =
+  let a, b, c, d, e, f = List.fold_right (fun elt (a, b, c, d, e, f) ->
+      let v1, v2, v3, v4, v5, v6 = ffrom elt in
+      v1 :: a, v2 :: b, v3 :: c, v4 :: d, v5 :: e, v6 :: f) a ([], [], [], [], [], []) in
+  conv ffrom fto (L.t6 a b c d e f)
+let c7 ffrom fto a =
+  let a, b, c, d, e, f, g = List.fold_right (fun elt (a, b, c, d, e, f, g) ->
+      let v1, v2, v3, v4, v5, v6, v7 = ffrom elt in
+      v1 :: a, v2 :: b, v3 :: c, v4 :: d, v5 :: e, v6 :: f, v7 :: g) a ([], [], [], [], [], [], []) in
+  conv ffrom fto (L.t7 a b c d e f g)
+let c8 ffrom fto a =
+  let a, b, c, d, e, f, g, h = List.fold_right (fun elt (a, b, c, d, e, f, g, h) ->
+      let v1, v2, v3, v4, v5, v6, v7, v8 = ffrom elt in
+      v1 :: a, v2 :: b, v3 :: c, v4 :: d, v5 :: e, v6 :: f, v7 :: g, v8 :: h) a ([], [], [], [], [], [], [], []) in
+  conv ffrom fto (L.t8 a b c d e f g h)
+let c9 ffrom fto a =
+  let a, b, c, d, e, f, g, h, i = List.fold_right (fun elt (a, b, c, d, e, f, g, h, i) ->
+      let v1, v2, v3, v4, v5, v6, v7, v8, v9 = ffrom elt in
+      v1 :: a, v2 :: b, v3 :: c, v4 :: d, v5 :: e, v6 :: f, v7 :: g, v8 :: h, v9 :: i) a ([], [], [], [], [], [], [], [], []) in
+  conv ffrom fto (L.t9 a b c d e f g h i)
+let c10 ffrom fto a =
+  let a, b, c, d, e, f, g, h, i, j = List.fold_right (fun elt (a, b, c, d, e, f, g, h, i, j) ->
+      let v1, v2, v3, v4, v5, v6, v7, v8, v9, v10 = ffrom elt in
+      v1 :: a, v2 :: b, v3 :: c, v4 :: d, v5 :: e, v6 :: f, v7 :: g, v8 :: h, v9 :: i, v10 :: j) a ([], [], [], [], [], [], [], [], [], []) in
+  conv ffrom fto (L.t10 a b c d e f g h i j)
--- src/dataframe.mli
+++ src/dataframe.mli
@@ -4,9 +4,16 @@ val merge_tups : 'a t -> 'b t -> ('a * 'b) t
 val conv : ('a -> 'b) -> ('b -> 'a) -> 'b t -> 'a t
 val length : _ t -> int
 val row : 'a t -> int -> 'a
+val map : ('a -> 'b) -> 'a t -> 'b list
+val rev_map : ('a -> 'b) -> 'a t -> 'b list
+val mapi : (int -> 'a -> 'b) -> 'a t -> 'b list
+val rev_mapi : (int -> 'a -> 'b) -> 'a t -> 'b list
 val iter : ('a -> unit) -> 'a t -> unit
 val iteri : (int -> 'a -> unit) -> 'a t -> unit
-val fold : ('b -> 'a -> 'b) -> 'b -> 'a t -> 'b
+val rev_iter : ('a -> unit) -> 'a t -> unit
+val rev_iteri : (int -> 'a -> unit) -> 'a t -> unit
+val fold_left : ('b -> 'a -> 'b) -> 'b -> 'a t -> 'b
+val fold_right : ('a -> 'b -> 'b) -> 'a t -> 'b -> 'b
 
 module type C = sig
   type 'a c
@@ -25,3 +32,13 @@ end
 module A : C with type 'a c := 'a Array.t
 module L : C with type 'a c := 'a List.t
 
+val c1 : ('a -> 'b) -> ('b -> 'a) -> 'a list -> 'a t
+val c2 : ('a -> 'b * 'c) -> ('b * 'c -> 'a) -> 'a list -> 'a t
+val c3 : ('a -> 'b * 'c * 'd) -> ('b * 'c * 'd -> 'a) -> 'a list -> 'a t
+val c4 : ('a -> 'b * 'c * 'd * 'e) -> ('b * 'c * 'd * 'e -> 'a) -> 'a list -> 'a t
+val c5 : ('a -> 'b * 'c * 'd * 'e * 'f) -> ('b * 'c * 'd * 'e * 'f -> 'a) -> 'a list -> 'a t
+val c6 : ('a -> 'b * 'c * 'd * 'e * 'f * 'g) -> ('b * 'c * 'd * 'e * 'f * 'g -> 'a) -> 'a list -> 'a t
+val c7 : ('a -> 'b * 'c * 'd * 'e * 'f * 'g * 'h) -> ('b * 'c * 'd * 'e * 'f * 'g * 'h -> 'a) -> 'a list -> 'a t
+val c8 : ('a -> 'b * 'c * 'd * 'e * 'f * 'g * 'h * 'i) -> ('b * 'c * 'd * 'e * 'f * 'g * 'h * 'i -> 'a) -> 'a list -> 'a t
+val c9 : ('a -> 'b * 'c * 'd * 'e * 'f * 'g * 'h * 'i * 'j) -> ('b * 'c * 'd * 'e * 'f * 'g * 'h * 'i * 'j -> 'a) -> 'a list -> 'a t
+val c10 : ('a -> 'b * 'c * 'd * 'e * 'f * 'g * 'h * 'i * 'j * 'k) -> ('b * 'c * 'd * 'e * 'f * 'g * 'h * 'i * 'j * 'k -> 'a) -> 'a list -> 'a t
